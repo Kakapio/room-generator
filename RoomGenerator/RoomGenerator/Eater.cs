b3ae8a8bd0bbb206f40ec643878cc8ffa892730b
Roshan Patel <ovicior@gmail.com>
2021-01-06 04:33:30 -0500

    Separated eater logic into different class, modified Game1.cs to have some static/const variables for use by eater. Generation is working at basic level.

---
+++ RoomGenerator/RoomGenerator/Eater.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace RoomGenerator
+{
+    public class Eater
+    {
+        public Vector2 Direction { get; private set; }
+        public Vector2 Position { get; private set; }
+
+        public Eater(Vector2 direction, Vector2 position)
+        {
+            Direction = direction;
+            Position = position;
+        }
+
+        /// <summary>
+        /// Move the Eater in its Direction by one step, remove whatever wall is in the way.
+        /// </summary>
+        public void Move()
+        {
+            var newPos = Position + Direction;
+            //Invalid direction, change direction and return.
+            if (newPos.X < 0 || newPos.Y < 0 || newPos.X > Game1.MapSize - 1 || newPos.Y > Game1.MapSize - 1
+            || Game1.CurrentFloors <= 0)
+            {
+                GenerateDirection();
+                return;
+            }
+
+            Position = newPos;
+            Game1.TileMap[(int) Position.X, (int) Position.Y] = Game1.TileType.Floor;
+            Game1.CurrentFloors--;
+            
+            GenerateDirection();
+        }
+
+        /// <summary>
+        /// Generate a new direction for the eater.
+        /// </summary>
+        /// <exception cref="ArgumentException"></exception>
+        public void GenerateDirection()
+        {
+            Random random = new Random();
+            int dir = random.Next(4);
+
+            switch (dir)
+            {
+                //Left
+                case 0:
+                    Direction = new Vector2(-1, 0);
+                    break;
+                //Up
+                case 1:
+                    Direction = new Vector2(0, 1);
+                    break;
+                //Right
+                case 2:
+                    Direction = new Vector2(1, 0);
+                    break;
+                //Down
+                case 3:
+                    Direction = new Vector2(0, -1);
+                    break;
+                default:
+                    throw new ArgumentException("Invalid value generated for new direction.");
+            }
+        }
+    }
+}
--- RoomGenerator/RoomGenerator/Game1.cs
+++ RoomGenerator/RoomGenerator/Game1.cs
@@ -9,83 +9,32 @@ namespace RoomGenerator
 {
     public class Game1 : Game
     {
-        enum TileType
+        public enum TileType
         {
             Floor,
             Wall
         }
         
-        class Eater
-        {
-            private Vector2 Direction { get; private set; }
-            private Vector2 Position { get; private set; }
-
-            public Eater(Vector2 direction, Vector2 position)
-            {
-                Direction = direction;
-                Position = position;
-            }
-
-            public void Move()
-            {
-                var newPos = Position + Direction;
-                if (newPos.X < 0 || newPos.Y < 0 || newPos.X > mapSize - 1 || newPos.Y > mapSize - 1)
-                {
-                    return;
-                }
-                
-                Position += Direction;
-            }
-
-            public void ChangeDirection()
-            {
-                Random random = new Random();
-                int dir = random.Next(4);
-
-                switch (dir)
-                {
-                    //Left
-                    case 0:
-                        Direction = new Vector2(-1, 0);
-                        break;
-                    //Up
-                    case 1:
-                        Direction = new Vector2(0, 1);
-                        break;
-                    //Right
-                    case 2:
-                        Direction = new Vector2(1, 0);
-                        break;
-                    //Down
-                    case 3:
-                        Direction = new Vector2(0, -1);
-                        break;
-                    default:
-                        throw new ArgumentException("Invalid value generated for new direction.");
-                }
-            }
-        }
+        //Variables that are accessed by the eater class.
+        public static TileType[,] TileMap { get; set; }
+        public const int MapSize = 100; //The square dimensions of the tile map in terms of tiles.
+        public const int MaxFloors = 1200;
+        public static int CurrentFloors { get; set; } = MaxFloors;
         
         private GraphicsDeviceManager graphics;
         private SpriteBatch spriteBatch;
-
-        private TileType[,] tilemap;
         private List<Eater> eaters;
-        private int mapSize = 100; //The square dimensions of the tile map in terms of tiles.
         private Texture2D floor;
         private Texture2D wall;
-        private int tileSize = 8; //The square dimensions of each tile sprite.
+        private const int TileSize = 8; //The square dimensions of each tile sprite.
         
-        //Parameters to modify map generation
-        private int maxFloorAmount = 120;
-
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
 
-            tilemap = new TileType[mapSize, mapSize];
+            TileMap = new TileType[MapSize, MapSize];
             eaters = new List<Eater>();
         }
 
@@ -98,16 +47,7 @@ namespace RoomGenerator
             graphics.PreferredBackBufferHeight = 1000;
             graphics.ApplyChanges();
             
-            //Pre-populate tilemap with base tile.
-            for (int i = 0; i < tilemap.GetLength(0); i++)
-            {
-                for (int j = 0; j < tilemap.GetLength(1); j++)
-                {
-                    tilemap[i, j] = TileType.Wall;
-                }
-            }
-            
-            eaters.Add(new Eater());
+            Reset();
         }
 
         protected override void LoadContent()
@@ -121,10 +61,19 @@ namespace RoomGenerator
         {
             base.Update(gameTime);
             
+            //Handle input
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-            
-            
+
+            if (Keyboard.GetState().IsKeyDown(Keys.R))
+                Reset();
+
+            foreach (var eater in eaters)
+            {
+                eater.Move();
+            }
+
+            Console.WriteLine($"Number floors created: {MaxFloors - CurrentFloors}");
         }
 
         protected override void Draw(GameTime gameTime)
@@ -134,26 +83,26 @@ namespace RoomGenerator
             GraphicsDevice.Clear(Color.SkyBlue);
             
             //Rendering to a RenderTarget and then later passing it to the back buffer.
-            RenderTarget2D target = new RenderTarget2D(GraphicsDevice, mapSize * tileSize, mapSize * tileSize);
+            RenderTarget2D target = new RenderTarget2D(GraphicsDevice, MapSize * TileSize, MapSize * TileSize);
             GraphicsDevice.SetRenderTarget(target);
             
             spriteBatch.Begin();
 
             //Draw each tile in the proper position.
-            for (int i = 0; i < tilemap.GetLength(0); i++)
+            for (int i = 0; i < TileMap.GetLength(0); i++)
             {
-                for (int j = 0; j < tilemap.GetLength(1); j++)
+                for (int j = 0; j < TileMap.GetLength(1); j++)
                 {
-                    switch (tilemap[i, j])
+                    switch (TileMap[i, j])
                     {
                         case TileType.Floor:
                             spriteBatch.Draw(floor, 
-                                new Rectangle(i * tileSize, j * tileSize, tileSize, tileSize), 
+                                new Rectangle(i * TileSize, j * TileSize, TileSize, TileSize), 
                                 Color.White);
                             break;
                         case TileType.Wall:
                             spriteBatch.Draw(wall, 
-                                new Rectangle(i * tileSize, j * tileSize, tileSize, tileSize), 
+                                new Rectangle(i * TileSize, j * TileSize, TileSize, TileSize), 
                                 Color.White);
                             break;
                         default:
@@ -169,6 +118,25 @@ namespace RoomGenerator
             spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.NonPremultiplied, SamplerState.PointClamp);
             spriteBatch.Draw(target, new Rectangle(0, 0, graphics.PreferredBackBufferWidth, graphics.PreferredBackBufferHeight), Color.White);
             spriteBatch.End();
+        }
+
+        private void Reset()
+        {
+            //Set all tiles back to wall.
+            for (int i = 0; i < TileMap.GetLength(0); i++)
+            {
+                for (int j = 0; j < TileMap.GetLength(1); j++)
+                {
+                    TileMap[i, j] = TileType.Wall;
+                }
+            }
+
+            CurrentFloors = MaxFloors;
+            eaters.Clear();
+            
+            eaters.Add(new Eater(new Vector2(0, 0), 
+                new Vector2(MapSize / 2, MapSize / 2)));
+            eaters[0].GenerateDirection();
         }
     }
 }
